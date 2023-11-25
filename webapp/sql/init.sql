d1b3cd1bffb2107f7be1794362b1f50724bc3f41
Tori Hara <toriclstr@gmail.com>
2023-11-24 19:59:56 -0800

    themes に user_id でインデックス追加

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -27,3 +27,5 @@ ALTER TABLE `users` auto_increment = 1;
 -- ALTER TABLE `livestream_tags`
 -- 	ADD INDEX `idx_livestream_id` (`livestream_id`),
 -- 	ADD UNIQUE `unique_tag_id_livestream_id` (`tag_id`, `livestream_id`);
+
+-- ALTER TABLE `themes` ADD INDEX `idx_user_id` (`user_id`);
