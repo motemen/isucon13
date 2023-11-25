c2d6f5ee05f9209d638bd725c9a1b7fb5b622536
Songmu <y.songmu@gmail.com>
2023-11-25 12:58:20 +0900

    fix user

--- webapp/go/user_handler.go
+++ webapp/go/user_handler.go
@@ -135,10 +135,10 @@ func postIconHandler(c echo.Context) error {
 	}
 
 	var username string
-	if err := dbConn.SelectContext(ctx, &username, "SELECT name FROM users WHERE id = ?", userID); err != nil {
+	if err := dbConn.GetContext(ctx, &username, "SELECT name FROM users WHERE id = ?", userID); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get username: "+err.Error())
 	}
-	if err := os.WriteFile("../public/icons/users"+username+".jpg", req.Image, 0666); err != nil {
+	if err := os.WriteFile("../public/icons/users/"+username+".jpg", req.Image, 0666); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to open file: "+err.Error())
 	}
 
